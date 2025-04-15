f5d49ec7cf19fb51d749b78da379ab4877051003
blawidris <blawidris@gmail.com>
2025-04-16 00:57:04 +0100

    side bar and header done

--- src/components/_layout/main.layout.tsx
+++ src/components/_layout/main.layout.tsx
@@ -6,15 +6,17 @@ import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
-
-  
   return (
     <SidebarProvider className="bg-">
       <AppSidebar variant="inset" />
       <SidebarInset className="bg-[#FBFBFC]">
         <SiteHeader />
+        <div className="p-10">
+          <Outlet />
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
