60c17ace78cc80fa277868ebbbef17ae3a05d20b
Ayodele Ayodeji <awesomedursky@gmail.com>
2025-04-16 18:15:16 +0100

    dash

--- src/components/_layout/main.layout.tsx
+++ src/components/_layout/main.layout.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
-
 import { AppSidebar } from "@/components/molecules/partials/app-sidebar";
 import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 
+import { Outlet } from "react-router";
 const MainLayout = () => {
   return (
     <SidebarProvider>
       <AppSidebar variant="inset" className="!bg-white" />
-      <SidebarInset> </SidebarInset>
+      <SidebarInset>
+        <Outlet />
+      </SidebarInset>
     </SidebarProvider>
   );
 };
--- src/components/templates/app/dashboard.tsx
+++ src/components/templates/app/dashboard.tsx
@@ -1,5 +1,35 @@
+import { columns } from "@/columns/testColumns";
+import { DataTable } from "@/components/atoms/table";
+
+type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+export const payments: Payment[] = [
+  {
+    id: "728ed52f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+];
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      DataTable
+      <DataTable columns={columns} data={payments} />
+    </div>
+  );
 };
 
 export default Dashboard;
