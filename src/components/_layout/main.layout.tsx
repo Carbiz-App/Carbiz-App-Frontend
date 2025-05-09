4b1e101ffe7784abda725319357c36433982e443
CIPEN-ICT <awesomedursky@gmail.com>
2025-05-09 20:32:28 +0100

    updated dashboard

--- src/components/_layout/main.layout.tsx
+++ src/components/_layout/main.layout.tsx
@@ -1,18 +1,22 @@
-import { Sidebar, SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import {
+  Sidebar,
+  SidebarInset,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
 
 import { AppSidebar } from "@/components/molecules/partials/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
-import {  Outlet, useNavigate } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { useMemo } from "react";
 import { useAuthStore } from "@/store/auth.store";
 
 const MainLayout = () => {
-  const token = useMemo(()=>localStorage.getItem('authToken'),[])
-  const {user} = useAuthStore()
-  const nav =useNavigate()
+  const token = useMemo(() => localStorage.getItem("authToken"), []);
+  const { user } = useAuthStore();
+  const nav = useNavigate();
 
-  if(!token &&!user){
-nav('/')
+  if (!token && !user) {
+    nav("/");
   }
   return (
     <SidebarProvider className="bg-">
