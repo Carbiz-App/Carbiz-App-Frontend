b8d77f6368973880b3f70846902f3c862affe548
CIPEN-ICT <awesomedursky@gmail.com>
2025-05-09 13:53:58 +0100

    update

--- src/components/_layout/main.layout.tsx
+++ src/components/_layout/main.layout.tsx
@@ -1,13 +1,23 @@
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Sidebar, SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 import { AppSidebar } from "@/components/molecules/partials/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
-import { Outlet } from "react-router";
+import {  Outlet, useNavigate } from "react-router";
+import { useMemo } from "react";
+import { useAuthStore } from "@/store/auth.store";
 
 const MainLayout = () => {
+  const token = useMemo(()=>localStorage.getItem('authToken'),[])
+  const {user} = useAuthStore()
+  const nav =useNavigate()
+
+  if(!token &&!user){
+nav('/')
+  }
   return (
     <SidebarProvider className="bg-">
       <AppSidebar />
+      {/* <Sidebar  /> */}
       <SidebarInset className="bg-[#FBFBFC]">
         <SiteHeader />
         <div className=" p-5  md:p-10">
